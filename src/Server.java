7b6cb68e3305efa10a9a7471f7d286cc290b94c2
simonljus <simon.ljus94@gmail.com>
2015-12-02 11:23:41 +0100

    added simple linked list

--- src/Server.java
+++ src/Server.java
@@ -2,17 +2,23 @@ import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.LinkedList;
         
-public class Server implements Hello {
-        
+public class Server implements Hello {   
     public Server() {}
 
     public String sayHello() {
         return "Hejhalloj";
     }
+    public LinkedList<String> getHelpList(){
+    return hl;	
+    }
         
     public static void main(String args[]) {
-        
+    	public LinkedList<String> hl = new Linkedlist();
+    	hl.add("Hej Tony");
+    	hl.add("Hej Douglas");
+    	
         try {
             Server obj = new Server();
             Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
