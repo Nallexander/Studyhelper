a199dcc678cf857a0844180ba14bf6000df8cae3
Tony <tonywang3334@gmail.com>
2015-12-10 09:33:48 +0100

    skrev lite trådgrejer jag tror vi skapar en tråd nu men jag har inte testat om det funkar

---
+++ src/ClientThread.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+public class ClientThread extends Thread  implements Runnable{
+  public Client client;
+  
+  public ClientThread(Client client){
+    this.client = client;
+  }
+  @Override
+  public void run(){
+    // TODO:
+    // polla number of questions om > 0 
+    // polla check claimed om   > 1
+    // decrementera number of questions om nånting blev claimed
+    System.out.println(Thread.currentThread().getId() + " THE THREAD ID");
+  }
+}
